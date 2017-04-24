e75f7dc34a69d2ad3f94a45ae738a602f13e8fb9
Mike van Genderen <mikevg466>
2017-04-24 14:06:30 -0400

    Adds curl using request module from npm and pushes all results to a 'done' function

--- bash.js
+++ bash.js
@@ -6,10 +6,14 @@ process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
   var cmdArgs = cmd.split(" ").slice(1);
   cmd = cmd.split(" ")[0];
+  var done = function(result){
+    process.stdout.write(result);
+    process.stdout.write("\n bash > ");
+  }
 
   // process.stdout.write('You typed: ' + cmd);
   var bash = require("./command.js");
-  if(bash.hasOwnProperty(cmd)) bash[cmd](cmdArgs);
+  if(bash.hasOwnProperty(cmd)) bash[cmd](done, cmdArgs);
   else process.stdout.write('Command: "' + cmd + '" does not exist.');
 
   // process.stdout.write('\nbash > ');
--- command.js
+++ command.js
@@ -2,77 +2,79 @@
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var fs = require('fs');
+var request = require('request');
 
 var Commands = module.exports = {};
-Commands.pwd = function(){
-  process.stdout.write(process.cwd());
-  process.stdout.write('\nbash > ');
+Commands.pwd = function(done){
+  done(process.cwd());
 }
-Commands.date = function(){
+Commands.date = function(done){
   var date = new Date();
-  process.stdout.write(weekday[date.getDay()] + ' ' + monthName[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear() + ' ' + date.toTimeString());
-  process.stdout.write('\nbash > ');
+  done(weekday[date.getDay()] + ' ' + monthName[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear() + ' ' + date.toTimeString());
 }
-Commands.ls = function(){
+Commands.ls = function(done){
   fs.readdir('.', function(err, files) {
     if (err) throw err;
+    var result = "";
     files.forEach(function(file) {
-      process.stdout.write(file.toString() + "\n");
+      result += (file.toString() + "\n");
     });
-    process.stdout.write('\nbash > ');
+    done(result);
   });
 }
-Commands.echo = function(args){
-  process.stdout.write(args.join(" "));
-  process.stdout.write('\n bash > ');
+Commands.echo = function(done, args){
+  done(args.join(" "));
 }
-Commands.cat = function(files){
+Commands.cat = function(done, files){
   if(!files.length){
-    process.stdout.write("Specify a file.");
-    process.stdout.write('\n bash > ');
+    done("Specify a file.");
   }
   else{
     var fileName = './' + files[0];
     fs.readFile(fileName, (err, data) => {
       if (err) throw err;
-      process.stdout.write(data);
-      process.stdout.write('\n bash > ');
+      done(data);
     }
     );
   }
-  process.stdout.write('\n bash > ');
 }
-Commands.head = function(files){
+Commands.head = function(done, files){
   if(!files.length){
-    process.stdout.write("Specify a file.");
-    process.stdout.write('\n bash > ');
+    done("Specify a file.");
   }
   else{
     var fileName = './' + files[0];
     fs.readFile(fileName, (err, data) => {
       if (err) throw err;
       var dataArr = String(data).split("\n");
-      for(var i = 0; i < 5; i++) process.stdout.write(dataArr[i] + '\n');
-      process.stdout.write('\n bash > ');
+      var result = ""
+      for(var i = 0; i < 5; i++) result += (dataArr[i] + '\n');
+      done(result);
     }
     );
   }
   //process.stdout.write('\n bash > ');
 }
-Commands.tail = function(files){
+Commands.tail = function(done, files){
   if(!files.length){
-    process.stdout.write("Specify a file.");
-    process.stdout.write('\n bash > ');
+    done("Specify a file.");
   }
   else{
     var fileName = './' + files[0];
     fs.readFile(fileName, (err, data) => {
       if (err) throw err;
       var dataArr = String(data).split("\n");
-      for(var i = dataArr.length - 5; i < dataArr.length; i++) process.stdout.write(dataArr[i] + '\n');
-      process.stdout.write('\n bash > ');
+      var result = "";
+      for(var i = dataArr.length - 5; i < dataArr.length; i++) result(dataArr[i] + '\n');
     }
     );
   }
   //process.stdout.write('\n bash > ');
+}
+
+
+Commands.curl = function(done, url){
+  var url = url[0];
+  request(url).pipe(process.stdout);
+  process.stdout.write('\n bash > ');
 }

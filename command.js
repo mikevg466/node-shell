34dfb6f457052d9e74f5d3177c755b6d3bc6dbf8
Mike van Genderen <mikevg466>
2017-04-24 12:43:41 -0400

    implements cat, head, and tail

--- command.js
+++ command.js
@@ -26,3 +26,53 @@ Commands.echo = function(args){
   process.stdout.write(args.join(" "));
   process.stdout.write('\n bash > ');
 }
+Commands.cat = function(files){
+  if(!files.length){
+    process.stdout.write("Specify a file.");
+    process.stdout.write('\n bash > ');
+  }
+  else{
+    var fileName = './' + files[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      process.stdout.write(data);
+      process.stdout.write('\n bash > ');
+    }
+    );
+  }
+  process.stdout.write('\n bash > ');
+}
+Commands.head = function(files){
+  if(!files.length){
+    process.stdout.write("Specify a file.");
+    process.stdout.write('\n bash > ');
+  }
+  else{
+    var fileName = './' + files[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      var dataArr = String(data).split("\n");
+      for(var i = 0; i < 5; i++) process.stdout.write(dataArr[i] + '\n');
+      process.stdout.write('\n bash > ');
+    }
+    );
+  }
+  //process.stdout.write('\n bash > ');
+}
+Commands.tail = function(files){
+  if(!files.length){
+    process.stdout.write("Specify a file.");
+    process.stdout.write('\n bash > ');
+  }
+  else{
+    var fileName = './' + files[0];
+    fs.readFile(fileName, (err, data) => {
+      if (err) throw err;
+      var dataArr = String(data).split("\n");
+      for(var i = dataArr.length - 5; i < dataArr.length; i++) process.stdout.write(dataArr[i] + '\n');
+      process.stdout.write('\n bash > ');
+    }
+    );
+  }
+  //process.stdout.write('\n bash > ');
+}
